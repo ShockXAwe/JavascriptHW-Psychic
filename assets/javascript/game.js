f9baa14d45bd3207a584abe8a10b0e2459888f0f
Virtual Beast <eddie.a.linares@gmail.com>
2019-03-29 15:58:57 -0500

    logic for userguess fixed, need reset of compguess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,11 @@ var possibleLetters = Array = ("abcdefghijklmnopqrstuvwxyz").split("");
 var computerPossibleLetters = Array = ("abcdefghijklmnopqrstuvwxyz").split("");
 // console.log(possibleLetters)
 var answer = [];
+// Variable for users guess
+let userGuess = [];
+// Creates random letter selector
+let computerChoice = computerPossibleLetters[Math.floor(Math.random() * computerPossibleLetters.length)];
+console.log(computerChoice);
 var remaining = 15;
 var wins = 0;
 var lost = 0;
@@ -17,26 +22,44 @@ var winsText = document.getElementById("wins-text");
 var lostText = document.getElementById("lost-text");
 
 // Create function for key input
-document.onkeypress = function (event) {
+document.onkeyup = function (event) {
     // Create variable for event key
-    var userGuess = event.key;
+    var userInput = event.key;
+    // Variable for new game (defualt to true so it starts as a new game)
+    var newGame = true
     // Creates random letter selector
-    var computerChoice = computerPossibleLetters[Math.floor(Math.random() * computerPossibleLetters.length)];
+    // var computerChoice = computerPossibleLetters[Math.floor(Math.random() * computerPossibleLetters.length)];
+    // Display user incorrect guess
+
+
+
+
+    // Logic for resetting guesses to blank
+    // document.onkeyup = function () {
+    //     if (newGame) {
+    //         userGuess();
+    //     }
+    // }
+
+
     // Logic for game
     // NEED!!!! logic to keep computer choice and not reset till 15 tries have been attempted
-    if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z"))
-        // (userGuess = possibleLetters)
-        {
-        if (userGuess === computerChoice) {
+    if ((userInput === "a") || (userInput === "b") || (userInput === "c") || (userInput === "d") || (userInput === "e") || (userInput === "f") || (userInput === "g") || (userInput === "h") || (userInput === "i") || (userInput === "j") || (userInput === "k") || (userInput === "l") || (userInput === "m") || (userInput === "n") || (userInput === "o") || (userInput === "p") || (userInput === "q") || (userInput === "r") || (userInput === "s") || (userInput === "t") || (userInput === "u") || (userInput === "v") || (userInput === "w") || (userInput === "x") || (userInput === "y") || (userInput === "z")) {
+        if (userInput === computerChoice) {
             wins++;
             remaining = 15;
+            userGuess = [];
+            computerChoice = computerChoice;
         }
-        else if (userGuess !== computerChoice) {
+        else if (userInput !== computerChoice) {
             remaining--;
+            userGuess.push(userInput);
         }
         if (remaining === 0) {
             lost++;
             remaining = 15;
+            userGuess = [];
+            computerChoice = computerChoice;
         }
 
         // Hides directions once user types
