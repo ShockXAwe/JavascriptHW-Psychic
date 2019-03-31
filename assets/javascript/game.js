724c939a4623a1b3caa32b8c88a93871517e744a
Secondary <eddie.a.linares@gmail.com>
2019-03-30 23:00:12 -0500

    cleaned up comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,6 @@ var possibleLetters = Array = ("abcdefghijklmnopqrstuvwxyz").split("");
 // Created seperate var to check if sharing the same array would be an issue...it wasn't..
 var computerPossibleLetters = Array = ("abcdefghijklmnopqrstuvwxyz").split("");
 // console.log(possibleLetters)
-var answer = [];
 // Variable for users guess
 var userGuess = [];
 // Creates random letter selector
@@ -42,7 +41,7 @@ document.onkeyup = function (event) {
             remaining--;
             userGuess.push(userInput);
         }
-        if (remaining === -1) {
+        if (remaining === 0) {
             lost++;
             remaining = 15;
             userGuess = [];
@@ -55,7 +54,6 @@ document.onkeyup = function (event) {
         directionsText.textContent = "";
         // Inserts game text
         remainingText.textContent = "Remaining guesses: " + remaining;
-        // NEED!!!! logic to display 15 userGuesses then refresh when correct or hit the 15 incorrect mark
         incorrectText.textContent = "Incorrect guesses: " + userGuess;
         winsText.textContent = "Total wins: " + wins;
         lostText.textContent = "Total losses: " + lost;
