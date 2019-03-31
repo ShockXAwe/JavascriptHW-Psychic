9f53903c0e7ba309a4aa352f791caa61025ce921
Secondary <eddie.a.linares@gmail.com>
2019-03-30 22:34:40 -0500

    fixed game logic for win/loss computerChoice

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,10 +6,11 @@ var computerPossibleLetters = Array = ("abcdefghijklmnopqrstuvwxyz").split("");
 // console.log(possibleLetters)
 var answer = [];
 // Variable for users guess
-let userGuess = [];
+var userGuess = [];
 // Creates random letter selector
-let computerChoice = computerPossibleLetters[Math.floor(Math.random() * computerPossibleLetters.length)];
-console.log(computerChoice);
+var computerChoice = computerPossibleLetters[Math.floor(Math.random() * computerPossibleLetters.length)];
+console.log(computerChoice)
+// console.log(computerChoice);
 var remaining = 15;
 var wins = 0;
 var lost = 0;
@@ -21,45 +22,33 @@ var incorrectText = document.getElementById("incorrectGuesses-text");
 var winsText = document.getElementById("wins-text");
 var lostText = document.getElementById("lost-text");
 
+
+
 // Create function for key input
 document.onkeyup = function (event) {
     // Create variable for event key
     var userInput = event.key;
-    // Variable for new game (defualt to true so it starts as a new game)
-    var newGame = true
-    // Creates random letter selector
-    // var computerChoice = computerPossibleLetters[Math.floor(Math.random() * computerPossibleLetters.length)];
-    // Display user incorrect guess
-
-
-
-
-    // Logic for resetting guesses to blank
-    // document.onkeyup = function () {
-    //     if (newGame) {
-    //         userGuess();
-    //     }
-    // }
-
-
     // Logic for game
-    // NEED!!!! logic to keep computer choice and not reset till 15 tries have been attempted
     if ((userInput === "a") || (userInput === "b") || (userInput === "c") || (userInput === "d") || (userInput === "e") || (userInput === "f") || (userInput === "g") || (userInput === "h") || (userInput === "i") || (userInput === "j") || (userInput === "k") || (userInput === "l") || (userInput === "m") || (userInput === "n") || (userInput === "o") || (userInput === "p") || (userInput === "q") || (userInput === "r") || (userInput === "s") || (userInput === "t") || (userInput === "u") || (userInput === "v") || (userInput === "w") || (userInput === "x") || (userInput === "y") || (userInput === "z")) {
         if (userInput === computerChoice) {
             wins++;
             remaining = 15;
             userGuess = [];
-            computerChoice = computerChoice;
+            computerChoice = computerPossibleLetters[Math.floor(Math.random() * computerPossibleLetters.length)];
+            console.log(computerChoice);
+            alert("Nice guess....You won this one!!")
         }
         else if (userInput !== computerChoice) {
             remaining--;
             userGuess.push(userInput);
         }
-        if (remaining === 0) {
+        if (remaining === -1) {
             lost++;
             remaining = 15;
             userGuess = [];
-            computerChoice = computerChoice;
+            computerChoice = computerPossibleLetters[Math.floor(Math.random() * computerPossibleLetters.length)];
+            console.log(computerChoice);
+            alert("LOL GG you lose GetGoodNoob!!")
         }
 
         // Hides directions once user types
@@ -70,6 +59,7 @@ document.onkeyup = function (event) {
         incorrectText.textContent = "Incorrect guesses: " + userGuess;
         winsText.textContent = "Total wins: " + wins;
         lostText.textContent = "Total losses: " + lost;
+        
     }
 
 }
